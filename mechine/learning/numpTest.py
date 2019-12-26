2fcea1eb3c4476b106c5d081d244c58ac5e7af40
xiaojun.zhang1 <xiaojun.zhang1@china.zhaogang.com>
2019-12-26 14:39:23 +0800

    日志值班脚本更新

---
+++ mechine/learning/numpTest.py
@@ -0,0 +1,4 @@
+import numpy
+
+arr = numpy.random.rand(4,4)
+print(arr)
--- myRequests/logRequests/myLogRequest.py
+++ myRequests/logRequests/myLogRequest.py
@@ -1,59 +1,48 @@
 import requests
 import json
-import itchat
+import sys
 
-cookies = dict(gr_user_id='ab9ffcc6-26be-45ca-9ef3-fffbc9e8836f',
-               sessionid="f578vcg52256w5jeygtqd29t9de7nfpk",
-               zg_sso_token="M5orrHJCEQfmd9RB8B75WXHlkkOsq0QTEtCeOY6msdi3YefY1hcijTy2z-JWi_kiy7Q4HsKYkrkMVKQfv7R94Q..",
-portalCookie="EgN_9fwUf1X11yRPZlWuR2Np0cebMW-gQSzeCRM5JnUd38u_xdeEyZSZ03A2U1HJsX3Sr--bQk4.",
-Hm_lvt_2897b5c5139f101b1546330e08d6d3ea="1561770362,1561943263,1562029530,1562118217",
-Hm_lpvt_2897b5c5139f101b1546330e08d6d3ea="1562118217",
-favor_system_alias="purchase.baseinfo.globalstock.api"
+input_param = sys.argv[1]+sys.argv[2]+sys.argv[3]+sys.argv[4]+sys.argv[5]+sys.argv[6]+sys.argv[7] + sys.argv[8] + sys.argv[9]
+# input_param = gr_user_id=ab9ffcc6-26be-45ca-9ef3-fffbc9e8836f; sessionid=0x8k3815mzca0qdlr6yuyprpjxtzbnkn; Hm_lvt_2897b5c5139f101b1546330e08d6d3ea=1576804163,1577063162,1577173890,1577322189; null#day_ana=10; null#isToday=Y; zg_sso_token=M5orrHJCEQfmd9RB8B75WXHlkkOsq0QTEtCeOY6msdi3YefY1hcijbLWDOHf_qe5a-HfttSJ5Rp3ReOWH7-k_A..; portalCookie=EgN_9fwUf1X11yRPZlWuR2Np0cebMW-gQSzeCRM5JnUd38u_xdeEyZSZ03A2U1HJsX3Sr--bQk4.; Hm_lpvt_2897b5c5139f101b1546330e08d6d3ea=1577329026; favor_system_alias=finance.basedata.platform.api
+input_arr = input_param.split(";")
+cookie_param = {}
+print("input_param" + "===>" + input_param)
+for input in input_arr:
+    key_value = input.split("=")
+    cookie_param[key_value[0]] = key_value[1]
 
-               )
-result = requests.post(
-"http://log.arch.zhaogangren.com/query/getApplogData/",
-    data = {
-        "product":"purchase.baseinfo.globalstock.api",
+print("cookie_param" + "===>" + str(input_param))
+
+systemArr = ['purchase.baseinfo.globalstock.api', 'purchase.baseinfo.globalstock.ui','purchase.purchasemanagement.selfmanagement.api',
+             'purchase.purchasemanagement.selfmanagement.ui', 'inventory.scm.management.api','inventory.scm.management.ui',
+             'inventory.scm.onsale.api','inventory.scm.onsale.ui', 'finance.basedata.platform.api','basicplatform.saas.report.service']
+
+for sysName in systemArr[0:]:
+    print("===========" + sysName + "===========")
+    param = {
+        "product":sysName,
         "runEnv":"40",
         "logLevel":"3",
-        "starttime":"2019-07-03 00:00:00",
-        "endtime":"2019-07-03 23:59:59",
+        "starttime":"2019-12-26 00:00:00",
+        "endtime":"2019-12-26 23:59:59",
         "topicBySdk":"false",
         "page":"1",
         "rows":"20",
         "order":"desc"
-                      },
-    cookies=cookies
-)
-
-jsonResult = json.loads(result.text)
-print(jsonResult["total"])
-
-if jsonResult["total"] > 0 :
-    notifyUrl = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=719142e4-f684-469c-8299-731a20a75716"
-    notifData = {
-    "msgtype": "text",
-    "text": {
-        "content": "error日志数量" + str(jsonResult["total"]),
-        "mentioned_list":["@all"]
-        }
     }
-## print(notifData)
-##  result = requests.post(notifyUrl, notifData)
-else:
-    print("no error!!")
 
+    result = requests.post(
+    "http://log.arch.zhaogangren.com/query/getApplogData/",
+        data = param,
+        cookies=cookie_param
+    )
 
-def after_login():
-    # 获取群聊列表
-    room = itchat.search_chatrooms(name="全村的希望")
-    print(len(room))
-    allRooms=itchat.get_chatrooms()
-    print("all" + str(len(allRooms)))
-    if len(room) > 0:
-        itchat.send_msg("error日志数量" + str(jsonResult["total"]), room[0]["UserName"])
+    jsonResult = json.loads(result.text)
+    rows = jsonResult["rows"]
+    total = jsonResult["total"]
+    if (total > 0) :
+        for item in rows[0:]:
+            content = item['content']
+            print(sysName + "              " + content)
 
-if __name__ == '__main__':
-    itchat.auto_login(loginCallback=after_login, hotReload=True)
-    itchat.run
+    print("【" + sysName + "】 error log num:" + str(total))
